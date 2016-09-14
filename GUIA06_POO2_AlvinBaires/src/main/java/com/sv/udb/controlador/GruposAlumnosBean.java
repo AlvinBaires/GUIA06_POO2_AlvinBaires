f966f51092f0f9f39ebb73958bc193fb80b0fddb
Laboratorio <Laboratorio@Laboratorio-03>
2016-09-14 17:53:23 -0600

    Mantenimiento FCDE

---
+++ GUIA06_POO2_AlvinBaires/src/main/java/com/sv/udb/controlador/GruposAlumnosBean.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.controlador;
+
+import com.sv.udb.ejb.*;
+import com.sv.udb.modelo.*;
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author Laboratorio
+ */
+@Named(value = "gruposAlumnosBean")
+@ViewScoped
+@ManagedBean
+public class GruposAlumnosBean implements Serializable{
+    @EJB
+    private GruposAlumnosFacadeLocal FCDEalumnosFacade;    
+    private GruposAlumnos objeGrupAlum;
+    private boolean guardar;
+    private List<GruposAlumnos> listGrupAlum = null;
+      
+    public GruposAlumnosBean() {
+    }
+    
+}
